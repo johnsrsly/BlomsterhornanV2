242e5ddb9376bf0c759295af5d2301cd78a1c771
johnsrsly <johnelarssonz@gmail.com>
2017-03-02 17:46:07 +0100

    -

---
+++ js/site.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 2000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $(window).bind('scroll', function () {
+        var navHeight = $(window).height() - 500;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-default',
+        offset: 80
+    });
+
+});
